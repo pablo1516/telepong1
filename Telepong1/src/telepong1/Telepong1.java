4011b5e8488e22f20c82be9ee9fe90923e47b343
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-03-07 18:53:12 -0500

    Add files via upload

---
+++ Telepong1/src/telepong1/Telepong1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telepong1;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author david
+ */
+public class Telepong1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmpong1 frmpong = new frmpong1();
+        frmpong.setVisible(true);
+        frmpong.setLocationRelativeTo(null);
+        frmpong.setSize(600, 400);
+        frmpong.setResizable(false);
+        frmpong.setTitle("Telepong");
+
+    }
+    
+}
